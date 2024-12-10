0532fb51dd7eb29f206f244450f872d5becef212
Daniela174-cam <danielamendez0023@gmail.com>
2024-12-10 07:48:04 -0500

    feat:sparkles

---
+++ EJERCICIO-_JS_ARIZANICOLL/js/app.js
@@ -0,0 +1,82 @@
+let letras = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+
+switch (letras) {
+    case "A":
+        console.log(".-")
+        break;
+    case "B":
+        console.log("-...")
+        break;
+    case "C":
+        console.log("-.-.")
+        break;
+    case "D":
+        console.log("-..")
+        break;
+    case "E":
+        console.log(".")
+        break;
+    case "F":
+        console.log("..-.")
+        break;
+    case "G":
+        console.log("--.")
+        break;
+    case "H":
+        console.log("....")
+        break;
+    case "I":
+        console.log("..")
+        break;
+    case "J":
+        console.log(".---")
+        break;
+    case "K":
+        console.log("-.-")
+        break;
+    case "L":
+        console.log(".-..")
+        break;
+    case "M":
+        console.log("--")
+        break;
+    case "N":
+        console.log("-.")
+        break;
+    case "O":
+        console.log("---")
+        break;
+    case "P":
+        console.log(".--.")
+        break;
+    case "Q":
+        console.log("--.-")
+        break;
+    case "R":
+        console.log(".-.")
+        break;
+    case "S":
+        console.log("...")
+        break;
+    case "T":
+        console.log("-")
+        break;
+    case "U":
+        console.log("..-")
+        break;
+    case "V":
+        console.log("...-")
+        break;
+    case "W":
+        console.log(".--")
+        break;
+    case "X":
+        console.log("-..-")
+        break;
+    case "Y":
+        console.log("-.--")
+        break;
+    case "Z":
+        console.log("--..")
+        break;
+}
