e2ab2bf957832bee66cdfc5f3021f09a131fd986
Daniela174-cam <danielamendez0023@gmail.com>
2024-12-10 08:17:20 -0500

    feat:sparkles

---
+++ EJERCICIO-_JS_ARIZANICOLL/EJERCICIO-_JS_ARIZANICOLL/js/app.js
@@ -0,0 +1,19 @@
+
+const MORSE_CODE_DICT = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.',
+    'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..',
+    'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.',
+    'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+    'Y': '-.--', 'Z': '--..', '0': '-----', '1': '.----', '2': '..---',
+    '3': '...--', '4': '....-', '5': '.....', '6': '-....', '7': '--...',
+    '8': '---..', '9': '----.', ' ': '/'
+};
+
+function textToMorse(text) {
+    return text.toUpperCase().split('').map(char => MORSE_CODE_DICT[char] || '').join(' ');
+}
+  
+const inputText = "a";
+const morseCode = textToMorse(inputText);
+console.log("Texto original:", inputText);
+console.log("Código Morse:", morseCode);
